95f63dfa16278e865bec918d188154051c2cee51
Khaled Zra <halo_434000@hotmail.com>
2024-04-30 23:52:04 +0200

    Changed to C# SignalR

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Project_B_Server.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
+// TODO Continue from Add a SignalR hub
+// https://learn.microsoft.com/en-us/aspnet/core/blazor/tutorials/signalr-blazor?view=aspnetcore-8.0&tabs=netcore-cli
---
+++ README.md
@@ -0,0 +1,21 @@
+# Project B Server
+Game server for client!
+
+WIP
+
+CMake tutorial how to build and run
+
+
+## build and run from scratch
+- mkdir test_build
+- cd .\test_build\
+- cmake ../../Project_B_Server
+- cmake --build .
+- cd Debug
+- .\Project_B_Server.exe
+
+
+## build and run from existing build
+- cmake --build .
+- cd Debug
+- .\Project_B_Server.exe
