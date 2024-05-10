7fc36bf77ce2465a80bf8c727094a02fbab5ad73
Khaled Zra <halo_434000@hotmail.com>
2024-05-10 12:52:07 +0200

    Added a temp fix to see if users can update the server

--- Project_B_Server/Hubs/ChatHub.cs
+++ Project_B_Server/Hubs/ChatHub.cs
@@ -9,8 +9,15 @@ public class ChatHub : Hub
         Console.WriteLine($"[{DateTime.Now}] - Game client connected!");
         return base.OnConnectedAsync();
     }
+    
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task SendPosition(string user, float x, float y)
+    {
+        await Clients.All.SendAsync("ReceivePosition", user, x, y);
+        Console.WriteLine($"[{DateTime.Now}] - {user} moved to ({x}, {y})");
     }
 }
