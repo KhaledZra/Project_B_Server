91b4613f68f0dbdd3ca436396e9dff68f350b9fe
Khaled Zra <halo_434000@hotmail.com>
2024-05-22 19:28:53 +0200

    SignalR can't be unit tested. ty microsoft.

--- Project_B_Server_Test/CustomLogger.cs
+++ Project_B_Server_Test/CustomLogger.cs
@@ -4,7 +4,7 @@ namespace Project_B_Server_Test;
 
 public static class CustomLogger
 { 
-    public static Serilog.Core.Logger Log = new LoggerConfiguration()
+    public static readonly Serilog.Core.Logger Log = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 }
